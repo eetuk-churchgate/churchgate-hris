0da93784b2771413855e92cde40ad0653c7d13e3
eetuk-churchgate <eetuk@churchgate.com>
2026-05-29 02:32:47 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from utils.chat_service import ChatService
 from utils.training_service import TrainingService
 from streamlit_quill import st_quill
 
-logo_icon = Path(__file__).parent / "churchgate-logo.png"
+logo_icon = Path(__file__).parent / "churchgate.logo.png"
 if logo_icon.exists():
     st.set_page_config(page_title="Churchgate Group HRIS", page_icon=str(logo_icon), layout="wide", initial_sidebar_state="expanded")
 else:
